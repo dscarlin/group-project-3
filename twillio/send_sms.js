22d8df14aa28207156e8738ef385fb0c98b42e71
J Lew <jtl@metricwise.net>
2019-07-06 13:57:54 -0400

    refactor send sms, create api route, update appController

--- twillio/send_sms.js
+++ twillio/send_sms.js
@@ -1,14 +1,21 @@
+require("dotenv").config()
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
-const accountSid = 'ACba635bef34bd665e3a5524406f0e3e7a';
-const authToken = '17790cecb9f4cc480463f81f086b6d1f';
-const client = require('twilio')(accountSid, authToken);
 
-client.messages
-  .create({
-     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-    from: '+19195253220',
-     to: '+12526462642'
-   })
-  .then(message => console.log(message.sid));
+function sendMessage(applicantNumber, message) {
+
+  const accountSid = 'ACba635bef34bd665e3a5524406f0e3e7a';
+  const authToken = '17790cecb9f4cc480463f81f086b6d1f';
+  const client = require('twilio')(accountSid, authToken);
+
+  client.messages
+    .create({
+      body: message,
+      from: '+19195253220',
+      to: applicantNumber
+    })
+    .then(message => console.log(message.sid));
+}
+
+export default sendMessage;
