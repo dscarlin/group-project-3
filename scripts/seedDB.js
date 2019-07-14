b0cbef1449949fb2f8f6e3e45fca6fb7df5fdaf6
J Lew <jtl@metricwise.net>
2019-07-14 15:34:19 -0400

    update seed script

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-// This file empties the Books collection and inserts the books below
-
 mongoose.connect(
   process.env.MONGODB_URI ||
-  "mongodb://localhost/reactreadinglist"
+  "mongodb://localhost/onthfly"
 );
 
 applicantSeed = require("../sampleData/applicants.json");
