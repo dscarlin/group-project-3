b31e5de1678b4e166be8903133136cdea001b5bc
David Carlin <dave.carlin86@gmail.com>
2019-07-15 16:06:14 -0400

    90 days on expire after

--- models/applicant.js
+++ models/applicant.js
@@ -50,7 +50,7 @@ const appSchema = new Schema({
 }, 
 {timestamps: true}
 );
-appSchema.index( { "createdAt": 1 }, { expireAfterSeconds: ms('30s') } )
+appSchema.index( { "createdAt": 1 }, { expireAfterSeconds: ms('90 days') } )
 
 
 const Applicant = mongoose.model("Applicant", appSchema);
